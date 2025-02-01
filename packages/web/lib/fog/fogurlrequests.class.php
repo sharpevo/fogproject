9b803beef646f8234437de0574ffedc2f64615f8
Tom Elliott <tommygunsster@gmail.com>
2025-02-01 06:38:30 -0600

    Just add the param header for the process method

--- packages/web/lib/fog/fogurlrequests.class.php
+++ packages/web/lib/fog/fogurlrequests.class.php
@@ -509,6 +509,7 @@ class FOGURLRequests extends FOGBase
      * @param string $callback   A callback to use if needed
      * @param string $file       A filename to use to download a file
      * @param mixed  $timeout    allow updating timeout values
+     * @param array  $header     Send specific headers.
      *
      * @return array
      */
